4c9641d4f5add323205d6b102edb1888a014cf71
Leonardo Araújo <araujoleonardo310@gmail.com>
2021-06-11 22:10:37 -0300

    Aulas adicionadas

---
+++ AULAS/funcaoSaida/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("%d\n", 603);
+    printf("%d\n", -603);
+    printf("%i\n", 603);
+    printf("%o\n", 603);
+    printf("%u\n", 603);
+    printf("%u\n", -603);
+    printf("%x\n", 603);
+    printf("%X\n", 603);
+    printf("%f\n", 603201.75);
+    printf("%e\n", 603201.75);
+    printf("%e\n", -603201.75);
+    printf("%E\n", 603201.75);
+    printf("%s\n", "String de exemplo");
+    printf("%c\n", 'T');
+
+    int a;
+    a = 15;
+
+    printf("\nO valor da variavel a eh %d", a);
+    return 0;
+}
